f39bc8c5963430ffac2feff6e382405697ae2b2c
jaianeoliveira581 <jaianeoliveira947@gmail.com>
2024-05-23 21:47:06 -0300

    versão 1.0

---
+++ main.py
@@ -0,0 +1,12 @@
+
+nomes = ['Mara', 'Paulo', 'Dayse, 'Sil', 'Placa', 'Biula']
+        
+indice = int(input('informe o indice que deseja alterar.'))
+indice -= 1
+
+nomes[indice] = input('infomre o novo nome: ')
+
+
+for nome in nomes:
+print(nome)
+
